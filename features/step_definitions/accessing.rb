fab9eef081732d4d494a88eb53ec7e5d63b6747c
diego <diego.sanches@gmail.com>
2014-02-04 11:07:31 -0300

    Test added - Enrolled user can sign in

--- features/step_definitions/accessing.rb
+++ features/step_definitions/accessing.rb
@@ -3,6 +3,13 @@ def normalize_action( action = '' )
   action.downcase!.sub!(' ', '_')
 end
 
+Given /^'(.*)' is enrolled$/ do |name|
+  User.create( :email => "#{name.downcase}@fenix.com", 
+    :password => "weakest_password",
+    :password_confirmation => "weakest_password" 
+  )
+end
+
 Given "There is not users" do
   expect(User.all).to eq([])
 end
@@ -12,11 +19,26 @@ And /^'(.*)' clicked '(.*)'$/ do |name, action|
   click_on( normalize_action(action) )
 end
 
+And /^'(.*)' visit logging page$/ do |name| 
+  visit( root_path )
+  click_on('log_in')
+end
+
+And "There not user logged in" do
+  visit( destroy_user_session_path )
+end
+
 When /^'(.+)' sign up$/ do |name|
   fill_in( 'Email', :with => "#{name.downcase}@fenix.com" )
   fill_in( 'Password', :with => "weakest_password" )
   fill_in( 'Password confirmation', :with => "weakest_password" )
   click_on( 'Sign up' ) 
+end
+
+When /^'(.+)' sign in$/ do |name|
+  fill_in( 'Email', :with => "#{name.downcase}@fenix.com" )
+  fill_in( 'Password', :with => "weakest_password" )
+  click_on( 'Sign in' ) 
 end
 
 When /^'(.+)' are logged in$/ do |name|
