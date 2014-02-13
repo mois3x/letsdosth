13b19a87b991c1e800ec30c7a4a3f500757e9afb
diego <diego.sanches@gmail.com>
2014-02-13 14:10:32 -0300

    Adding aceptance test for Complaints index

--- features/complaints.feature
+++ features/complaints.feature
@@ -2,7 +2,8 @@ Feature: Users can browse complaints list
   Users, even those who are neither logged in nor enrolled, can browse complaints
 
         Scenario: User access complaints list
-          Given 'chad complaint' and 'john complaint' were written by authors
+          Given 'chad complaint' was written by the author
+          And   'john complaint' was written by the author
           When  User visit 'complaints index'
           Then  User sees 'chad complaints' and it's advocators
           And   User sees 'john complaints' and it's advocators
--- features/step_definitions/complaints.rb
+++ features/step_definitions/complaints.rb
@@ -1,16 +1,33 @@
-Given /^'(.*)' was written by author$/ do |complaint_title|
-  name = complaint_title.split( " " ).first
-  author = User.create( :email => "#{name.downcase}@domain.com", 
-    :password => "weakest_password",
-    :password_confirmation => "weakest_password" 
+def complaint_author_info(complaint_title = " " ) 
+  name = complaint_title.split( " " ).first.downcase
+  return { :name => name, :email => "#{name.downcase}@domain.com",
+    :password => 'weakest_password' }
+end
+
+Given /^'(.*)' was written by the author$/ do |complaint_title|
+  info = complaint_author_info(complaint_title)
+  author = User.create( :email => info[:email], 
+    :password => info[:password], 
+    :password_confirmation => info[:password] 
   )
   Complaint.create( :title => complaint_title, :body => Faker::Lorem.sentences,
     :author => author );
 end
 
-When "User visit complaints" do
+When "User visit 'complaints index'" do
   visit( complaints_index_path )
 end
 
-Then "User sees '(.*)' and it's advocators" do |complaint_title|
+Then /^User sees '(.*)' and it's advocators$/ do |complaint_title|
+  info = complaint_author_info(complaint_title)
+  author = User.where( email: info[:email] ).first
+  complaint = Complaint.includes(:users).where( 
+    "signatures.user_id = #{author.id}" ).first
+ 
+  page.should have_content( complaint.body )
+  page.should have_content( complaint.title )
+
+  complaint.users(true).each do |u| 
+    page.should have_content( u )
+  end
 end
