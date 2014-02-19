2f6059214be1c35632fb30783fc7aa7d916baeb8
diego <diego.sanches@gmail.com>
2014-02-19 15:56:29 -0300

    Navigation links added and version information

---
+++ app/models/version.rb
@@ -0,0 +1,22 @@
+class Version
+  def self.major
+    0
+  end
+
+  def self.minor
+    0
+  end
+
+  def self.patch 
+    0
+  end
+
+  def self.build
+    ENV['BUILD_NUMBER']
+  end
+
+  def self.as_string
+    "#{self.major}.#{self.minor}.#{self.patch}+#{self.build}"
+  end
+
+end
