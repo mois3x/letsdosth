2c1cf5082485a11186cc8ed05077ca2f4e965372
diego <diego.sanches@gmail.com>
2014-05-13 17:56:47 -0300

    Dropzone section added (WiP)

--- app/assets/javascripts/complaints.js
+++ app/assets/javascripts/complaints.js
@@ -1,31 +1,41 @@
+//= require dropzone
+
+function update_advocators( action_button ) {
+  $(action_button).click( function(event) {
+    var form = $(this).closest('form');
+    var post = $.post( form.attr('action'), form.serialize() );
+
+    post.done( function(  data, textStatus, jqXHR  ) {
+      var content = $('#comp_' + data.id).find('.comp-advocators')
+      content.empty()
+      jQuery.each( data.advocators.reverse(), function( i, email ) {
+        content.append( '<span><p>' + email + '</p></span>' );
+      });
+    });
+
+    post.fail( function( jqXHR, textStatus, errorThrown) {
+      console.debug( "errorThrown: " + errorThrown );
+      console.debug( "textStatus: " + textStatus );
+    });
+
+    event.preventDefault();
+  });
+};
+
+function dropzone_setup() {
+  var zone = $('#zone');
+  var form_url = zone.closest('form').prop('action');
+  zone.dropzone( { url: form_url } );
+};
+
 
 $(document).ready( function() {
-
-  function update_advocators( action_button ) {
-    $(action_button).click( function(event) {
-      var form = $(this).closest('form');
-      var post = $.post( form.attr('action'), form.serialize() );
-
-      post.done( function(  data, textStatus, jqXHR  ) {
-        var content = $('#comp_' + data.id).find('.comp-advocators')
-        content.empty()
-        jQuery.each( data.advocators.reverse(), function( i, email ) {
-          content.append( '<span><p>' + email + '</p></span>' );
-        });
-      });
-
-      post.fail( function( jqXHR, textStatus, errorThrown) {
-        console.debug( "errorThrown: " + errorThrown );
-        console.debug( "textStatus: " + textStatus );
-      });
-
-      event.preventDefault();
-    });
-  };
+  Dropzone.autoDiscover = false;
 
   update_advocators( '.advocate' );
   update_advocators( '.relinquish' );
 
+  dropzone_setup();
 
 });
 
